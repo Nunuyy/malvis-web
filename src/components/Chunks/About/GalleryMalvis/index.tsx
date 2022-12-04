9e4701ee0d5e1856220983f15f20021d5d318be0
Nunuyy <amalianuy01@gmail.com>
2022-12-04 17:00:50 +0700

    fix: another lieur

--- src/components/Chunks/About/GalleryMalvis/index.tsx
+++ src/components/Chunks/About/GalleryMalvis/index.tsx
@@ -1,6 +1,7 @@
 import tw, { styled } from 'twin.macro'
 import Image from 'next/image'
-import React, { Fragment, useRef } from 'react'
+import React, { Fragment } from 'react'
+//import React, {  useRef } from 'react'
 import Slider, { Settings } from 'react-slick'
 import { Container } from '@/components/Layouts'
 import { useEffect } from 'react'
@@ -11,7 +12,7 @@ import 'aos/dist/aos.css'
 // import GalleryCard from '@/components/Card/Gallery'
 
 const WrapperSlider = styled.div`
-  ${tw`flex place-content-center items-center`}
+  ${tw`flex items-center place-content-center`}
   .slick-slider {
     overflow: hidden;
   }
@@ -35,19 +36,20 @@ export const GalleryMalvis: React.FC = () => {
     autoplaySpeed: 5000,
     centerMode: true
   }
-  const sliderRef = useRef<any>(null)
-  const gotoNext = () => {
-    sliderRef.current.slickNext()
-  }
-  const gotoPrev = () => {
-    sliderRef.current.slickPrev()
-  }
+  // const sliderRef = useRef<any>(null)
+  // const gotoNext = () => {
+  //   sliderRef.current.slickNext()
+  // }
+  // const gotoPrev = () => {
+  //   sliderRef.current.slickPrev()
+  // }
   return (
     <Container tag={'div'} tw="grid grid-cols-1  md:grid-cols-3 grid-rows-1">
       <Fragment>
         <div>
           <WrapperSlider data-aos="fade-up">
-            <Slider {...setting} ref={sliderRef}>
+            <Slider {...setting}>
+              {/* ref={sliderRef} */}
               {[...Array(1)].map(() => (
                 <div key={Math.random()}>
                   <div>
@@ -180,13 +182,13 @@ export const GalleryMalvis: React.FC = () => {
               ))}
             </Slider>
           </WrapperSlider>
-          <div tw="w-full flex space-x-3 items-center place-content-center">
+          {/* <div tw="w-full flex space-x-3 items-center place-content-center">
             <div className="flex w-20 col-span-4 gap-4 mt-3">
               <button className="button" onClick={() => gotoPrev()}>
                 <Image
                   src="/assets/images/arrow-down-circle.svg"
                   alt=""
-                  className="w-full object-contain"
+                  className="object-contain w-full cursor-pointer"
                   width={170}
                   height={170}
                 />
@@ -195,13 +197,13 @@ export const GalleryMalvis: React.FC = () => {
                 <Image
                   src="/assets/images/arrow-up-circle.svg"
                   alt=""
-                  className="w-full object-contain"
+                  className="object-contain w-full cursor-pointer"
                   width={170}
                   height={170}
                 />
               </button>
             </div>
-          </div>
+          </div> */}
         </div>
         <div tw="md:my-32">
           <div tw="md:my-auto grid sm:py-8 py-8 md:py-0 place-content-center items-center" data-aos="fade">
@@ -219,7 +221,8 @@ export const GalleryMalvis: React.FC = () => {
       <Fragment>
         <div>
           <WrapperSlider data-aos="fade-down">
-            <Slider {...setting} ref={sliderRef}>
+            <Slider {...setting}>
+              {/* ref={sliderRef} */}
               {[...Array(1)].map(() => (
                 <div>
                   <div>
@@ -296,13 +299,13 @@ export const GalleryMalvis: React.FC = () => {
               ))}
             </Slider>
           </WrapperSlider>
-          <div tw="w-full flex space-x-3 items-center place-content-center">
+          {/* <div tw="w-full flex space-x-3 items-center place-content-center">
             <div className="flex w-20 col-span-4 gap-4 mt-3">
               <button className="button" onClick={() => gotoPrev()}>
                 <Image
                   src="/assets/images/arrow-down-circle.svg"
                   alt=""
-                  className="w-full object-contain"
+                  className="object-contain w-full cursor-pointer"
                   width={170}
                   height={170}
                 />
@@ -311,13 +314,13 @@ export const GalleryMalvis: React.FC = () => {
                 <Image
                   src="/assets/images/arrow-up-circle.svg"
                   alt=""
-                  className="w-full object-contain"
+                  className="object-contain w-full cursor-pointer"
                   width={170}
                   height={170}
                 />
               </button>
             </div>
-          </div>
+          </div> */}
         </div>
       </Fragment>
     </Container>
