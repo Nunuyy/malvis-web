066be5a1d9d1817b7247c8374c656d6699834361
Nunuyy <amalianuy01@gmail.com>
2022-12-04 17:20:24 +0700

    fix: about page

--- src/components/Chunks/About/AboutMalvis/index.tsx
+++ src/components/Chunks/About/AboutMalvis/index.tsx
@@ -7,9 +7,9 @@ import tw, { styled } from 'twin.macro'
 const Background = styled.div`
   ${tw`bg-[#d9d9d9]`}
 `
-const Content = styled.div`
-  ${tw`flex flex-col items-center justify-between w-full space-y-3 lg:flex-row lg:space-y-0 lg:py-16 md:mb-0 mt-32 md:mt-0`}
-`
+// const Content = styled.div`
+//   ${tw`flex flex-col items-center justify-between w-full space-y-3 lg:flex-row lg:space-y-0 lg:py-16 md:mb-0 mt-32 md:mt-0`}
+// `
 const ContentLeft = styled.div`
   ${tw``}
 `
@@ -25,21 +25,19 @@ export const AboutMalvis: React.FC = () => {
     <>
       <Element name="aboutmalvis" className="element">
         <Background className="bg-no-repeat bg-center bg-cover">
-          <Container tag={'div'} tw="p-5 md:p-14">
-            <Content>
-              <ContentLeft>
-                <ContentLeftService>
-                  <p tw="text-center text-malvisblue font-bold md:text-4xl lg:my-5  mb-2 md:mb-0">ABOUT US</p>
-                  <p tw="text-[#232323] -translate-y-20 mb-2  md:mb-5 md:text-center sm:text-justify text-sm">
-                    Local Counsel, Globally Connected Malvis Attorneys at Law Law Office founded in 2022 with a wide range of
-                    its Founding Partners experiences, which are &nbsp;
-                    <b>Zubet Rizal.S.H., Reza Satria Kinayungan.S.H, CTL., Gading Yonggar Ditya.S.H.</b> Supported with
-                    strong analitycal of Lawyers, Paralegal, as well as Field Administration Staff who will be incharged as a
-                    solid team to provide many legal consultation to give the best service for valuable clients.
-                  </p>
-                </ContentLeftService>
-              </ContentLeft>
-            </Content>
+          <Container tw="p-5 md:p-14">
+            <ContentLeft>
+              <ContentLeftService>
+                <p tw="text-center text-malvisblue font-bold md:text-4xl lg:my-5  mb-2 md:mb-0">ABOUT US</p>
+                <p tw="text-[#232323] mb-2  md:mb-5 md:text-center text-justify text-sm">
+                  Local Counsel, Globally Connected Malvis Attorneys at Law Law Office founded in 2022 with a wide range of
+                  its Founding Partners experiences, which are &nbsp;
+                  <b>Zubet Rizal.S.H., Reza Satria Kinayungan.S.H, CTL., Gading Yonggar Ditya.S.H.</b> Supported with strong
+                  analitycal of Lawyers, Paralegal, as well as Field Administration Staff who will be incharged as a solid
+                  team to provide many legal consultation to give the best service for valuable clients.
+                </p>
+              </ContentLeftService>
+            </ContentLeft>
           </Container>
         </Background>
       </Element>
