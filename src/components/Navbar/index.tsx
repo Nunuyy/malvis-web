8307f398ff1aab14c055108af63f3e2de8e219dc
Nunuyy <amalianuy01@gmail.com>
2022-12-06 16:51:01 +0700

    fix: logo nav

--- src/components/Navbar/index.tsx
+++ src/components/Navbar/index.tsx
@@ -51,7 +51,7 @@ export const Navbar: FC<LinkProps> = ({ withScrolled = true }) => {
           <div tw="relative w-[155px] h-auto justify-start cursor-pointer md:hidden flex">
             <Image
               unoptimized={true}
-              src={'/assets/icons/malvis-logo.png'}
+              src={'/assets/icons/malvis-logo.svg'}
               width={100}
               height={100}
               onClick={() => router?.push('/')}
